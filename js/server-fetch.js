1b2d7297369139bdecf8a53115396848dbb41d6a
DK <darshankhot@programmer.net>
2021-08-27 21:55:55 +0530

    added put and delete features in ajax

--- js/server-fetch.js
+++ js/server-fetch.js
@@ -2,11 +2,10 @@ const submitRequest = (type, url, data = null, isAsync = true) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(type, `http://localhost:3000/${url}`, isAsync, data);
-    if(['post', 'patch', 'put'].indexOf(type.toLowerCase()) !== -1) {
-      xhr.setRequestHeader('Content-Type', 'application/json');
+    if (["post", "patch", "put"].indexOf(type.toLowerCase()) !== -1) {
+      xhr.setRequestHeader("Content-Type", "application/json");
       xhr.send(data);
-    }
-    else {
+    } else {
       xhr.send();
     }
 
@@ -31,24 +30,51 @@ document.querySelector(".get").addEventListener("click", async function () {
   }
 });
 
-document.querySelector(".post-req").addEventListener("click", async function () {
+document
+  .querySelector(".post-req")
+  .addEventListener("click", async function () {
+    try {
+      const empData = JSON.stringify({
+        name: "Someoneelse",
+        profile: "1",
+        gender: "female",
+        department: ["HR", "Finance", "Other"],
+        salary: 23000,
+        startdate: "16 Aug 2021",
+        notes: "some note is given here",
+      });
+      const data = await submitRequest("POST", "employees/", empData);
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+document.querySelector(".put-req").addEventListener("click", async function () {
   try {
     const empData = JSON.stringify({
-      "name": "Someoneelse",
-      "profile": "1",
-      "gender": "female",
-      "department": [
-        "HR",
-        "Finance",
-        "Other"
-      ],
-      "salary": 23000,
-      "startdate": "16 Aug 2021",
-      "notes": "some note is given here"
+      name: "Someother",
+      profile: "3",
+      gender: "female",
+      department: ["HR", "Finance", "Other"],
+      salary: 25000,
+      startdate: "16 Aug 2021",
+      notes: "some note is given here",
     });
-    const data = await submitRequest("POST", "employees/", empData);
+    const data = await submitRequest("PUT", "employees/1", empData);
     console.log(data);
   } catch (err) {
     console.error(err);
   }
 });
+
+document
+  .querySelector(".delete-req")
+  .addEventListener("click", async function () {
+    try {
+      const data = await submitRequest("DELETE", "employees/1");
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  });
