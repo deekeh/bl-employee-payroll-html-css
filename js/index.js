1643fc7be9f63ae9c252aaca91b299bfd698acb6
DK <darshankhot@programmer.net>
2021-08-28 21:15:05 +0530

    added edit and delete functionality in employee payroll website using ajax

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-const createTableRow = ({profile, name, gender, department, salary, startDate, id}) => {
+import { submitRequest } from "./service/server-fetch.js";
+
+const createTableRow = ({
+  profile,
+  name,
+  gender,
+  department,
+  salary,
+  startDate,
+  id,
+}) => {
   return `
     <tr>
       <td class="e-profile">
@@ -11,7 +21,9 @@ const createTableRow = ({profile, name, gender, department, salary, startDate, i
         ${gender}
       </td>
       <td class="e-department">
-        ${department.map(d => (`<span class="department-pill">${d}</span>`)).join('')}
+        ${department
+          .map((d) => `<span class="department-pill">${d}</span>`)
+          .join("")}
       </td>
       <td class="e-salary">
         &#8377; ${salary.toLocaleString()}
@@ -27,7 +39,7 @@ const createTableRow = ({profile, name, gender, department, salary, startDate, i
             </svg>
           </span>
         </button>
-        <button onclick="editEmployee(${id})" class="action-button" type="button">
+        <button onclick="editEmployee(${id});" class="action-button" type="button">
           <span class="delete icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#658292" class="bi bi-pencil-fill" viewBox="0 0 16 16">
               <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
@@ -38,13 +50,18 @@ const createTableRow = ({profile, name, gender, department, salary, startDate, i
       </td>
     </tr>
   `;
-}
+};
 
-function populateTable() {
+export async function populateTable() {
   let employeeJsonArray = [];
   // save all employees to employeJsonArray
-  if (window.localStorage.getItem('empData')) {
-    JSON.parse(window.localStorage.getItem('empData')).forEach(e => {
+  try {
+    const data = await submitRequest("GET", "employees/");
+
+    // uncomment to log updated data
+    // console.log(data);
+
+    data.forEach((e) => {
       employeeJsonArray.push({
         profile: e.eProfile,
         name: e.eName,
@@ -56,32 +73,37 @@ function populateTable() {
       });
     });
     // display number of employees
-    document.getElementById("employee-number").innerHTML = JSON.parse(window.localStorage.getItem('empData')).length;
+    document.getElementById("employee-number").innerHTML =
+      employeeJsonArray.length;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    // display employees in table by creating new rows
+    document.querySelector("#e-table-body").innerHTML = "";
+    employeeJsonArray.forEach((employeeJson) => {
+      document.querySelector("#e-table-body").innerHTML +=
+        createTableRow(employeeJson);
+    });
   }
-  else document.getElementById("employee-number").innerHTML = 0;
-
-  // display employees in table by creating new rows
-  document.querySelector("#e-table-body").innerHTML = '';
-  employeeJsonArray.forEach(employeeJson => {
-    document.querySelector("#e-table-body").innerHTML += createTableRow(employeeJson);
-  });
 }
 
-window.onload = function() {
+window.onload = function () {
   populateTable();
-}
+};
 
+// https://stackoverflow.com/a/44591205
 // delete a particular employee using id
-function deleteEmployee(id) {
-  let employees = JSON.parse(window.localStorage.getItem('empData'));
-  let newEmployees = employees.filter(e => e.id !== id); // return all objects except the object to be deleted
-  // uncomment to log the new employee details array
-  // console.log(newEmployees);
-  window.localStorage.setItem('empData', JSON.stringify(newEmployees));
-  populateTable(); // recreate the table
-}
+window.deleteEmployee = async (id) => {
+  try {
+    await submitRequest("DELETE", `employees/${id}`);
+    populateTable();
+  } catch (err) {
+    console.error(err);
+  }
+};
 
+// https://stackoverflow.com/a/44591205
 // redirecting to registration page with employee id in query string to edit particular employee details
-function editEmployee(id) {
-  window.location.href = "./employee-registration.html?edit="+id;
-}
+window.editEmployee = (id) => {
+  window.location.href = "./employee-registration.html?edit=" + id;
+};
