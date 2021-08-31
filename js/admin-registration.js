0385c4693b1c1561774c071f4970c2b364de10a6
DK <darshankhot@programmer.net>
2021-08-31 18:01:27 +0530

    added admin registration with json-server

---
+++ js/admin-registration.js
@@ -0,0 +1,85 @@
+import { submitRequest } from "./service/server-fetch.js";
+
+let redirectUrl = "./";
+
+const checkInputs = async (email, phone, password) => {
+  return new Promise(async (resolve, reject) => {
+    // empty value check
+    if (!email) reject("email_required");
+    else if (!phone) reject("phone_required");
+    else if (!password) reject("password_required");
+
+    // regex check
+    else if (!/^([a-zA-Z0-9]+([.][a-zA-Z0-9]+)*)[@]([a-zA-Z0-9]+([.][a-zA-Z]{2,})+)$/.test(email)) reject("email_format");
+    else if (!/^[9][1][ ][6789][\d]{9}$/.test(phone)) reject("phone_format");
+
+    else try {
+      const searchedEmployee = await submitRequest("GET", `credentials?phone=${phone}`);
+      if (searchedEmployee.length !== 0) reject("existing_phone");
+      else {
+        await submitRequest("POST", "credentials/", JSON.stringify({email,phone, password}));
+        resolve(true);
+      }
+    }
+    catch(err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+document.querySelector(".login-form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const
+    email =  e.target.email.value,
+    phone = e.target.phone.value,
+    password = e.target.password.value
+  ;
+  document.querySelectorAll(".form-content>input").forEach(el => el.classList.remove("error"));
+
+  try {
+    const checkUser = await checkInputs(email, phone, password);
+    if (checkUser) window.location.href = './login.html';
+  }
+  catch(err) {
+    switch(err) {
+      case "email_required":
+        document.querySelector(".form-content>.email").classList.add("error");
+        document.querySelector(".form-content>.email-error").innerHTML = "Required";
+        break;
+        
+      case "phone_required":
+        document.querySelector(".form-content>.phone").classList.add("error");
+        document.querySelector(".form-content>.phone-error").innerHTML = "Required";
+        break;
+      case "password_required":
+        document.querySelector(".form-content>.password").classList.add("error");
+        document.querySelector(".form-content>.password-error").innerHTML = "Required";
+        break;
+      case "email_format":
+        document.querySelector(".form-content>.email").classList.add("error");
+        document.querySelector(".form-content>.email-error").innerHTML = "Invalid format [name@example.com]";
+        break;
+      case "phone_format":
+        document.querySelector(".form-content>.phone").classList.add("error");
+        document.querySelector(".form-content>.phone-error").innerHTML = "Invalid format [91 XXXXXXXXXX]";
+        break;
+      case "existing_phone":
+        document.querySelector(".form-content>.phone").classList.add("error");
+        document.querySelector(".form-content>.phone-error").innerHTML = "Phone already registered";
+        break;
+      default:
+        console.error(err);
+        break;
+    }
+  }
+});
+
+window.onload = function () {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  redirectUrl = params.redirect ? decodeURI(params.redirect) : "./";
+  // console.log(redirectUrl);
+  if (window.localStorage.getItem("loginkey"))
+    window.location.href = redirectUrl;
+};
--- js/login.js
+++ js/login.js
@@ -1,38 +1,21 @@
 import { submitRequest } from "./service/server-fetch.js";
 
-let redirectUrl = './';
+let redirectUrl = "./";
 
 const checkCredentials = (phone, password) => {
   return new Promise(async (resolve, reject) => {
     if (!phone) reject("phone_required");
     if (!password) reject("password_required");
-    if (!/^[9][1][ ][6789][\d]{9}$/.test(phone)) reject ("phone_format")
+    if (!/^[9][1][ ][6789][\d]{9}$/.test(phone)) reject("phone_format");
 
     try {
-      const userDetails = await submitRequest("GET", "credentials/");
-      // let credentials = JSON.parse(userDetails;
-      if (phone == userDetails.phone && password == userDetails.password)
-      resolve(true);
+      const userDetails = await submitRequest("GET", `credentials/?phone=${phone}&password=${password}`);
+      if (userDetails.length !== 0)
+        resolve(true);
       else reject("input_mismatch");
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
     }
-
-    // const xhr = new XMLHttpRequest();
-    // xhr.open("GET", "http://localhost:3000/credentials", true);
-    // xhr.send();
-
-    // xhr.onreadystatechange = function () {
-    //   if (this.readyState == 4) {
-    //     console.log(this.readyState);
-    //     let credentials = JSON.parse(this.responseText);
-    //     console.log(credentials);
-    //     if (phone == credentials.phone && password == credentials.password)
-    //       resolve(true);
-    //     else reject("input_mismatch");
-    //   }
-    // };
   });
 };
 
@@ -40,7 +23,7 @@ document
   .querySelector(".login-form")
   .addEventListener("submit", async function (e) {
     e.preventDefault();
-    console.log(e.target.password.value);
+    // console.log(e.target.password.value);
 
     document.querySelectorAll(".form-content>input").forEach((el) => {
       el.classList.remove("error");
@@ -57,13 +40,13 @@ document
           el.classList.remove("error");
           document.querySelector(".login-container").classList.remove("emerge");
           document.querySelector(".login-container").classList.add("retract");
-          window.localStorage.setItem('loginkey', "true");
-          setTimeout(function() {
+          window.localStorage.setItem("loginkey", "true");
+          setTimeout(function () {
             window.location.href = redirectUrl;
           }, 700);
         });
       }
-      console.log(checkResult);
+      // console.log(checkResult);
     } catch (err) {
       switch (err) {
         default:
@@ -96,10 +79,11 @@ document
   });
 
 // check if the user is already logged in on page load and redirect to home if so
-window.onload = function() {
+window.onload = function () {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const params = Object.fromEntries(urlSearchParams.entries());
-  redirectUrl = (params.redirect || './');
-  console.log(redirectUrl)
-  if (window.localStorage.getItem('loginkey')) window.location.href = redirectUrl;
-}
+  redirectUrl = params.redirect || "./";
+  console.log(redirectUrl);
+  if (window.localStorage.getItem("loginkey"))
+    window.location.href = redirectUrl;
+};
